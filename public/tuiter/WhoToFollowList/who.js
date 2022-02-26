ec2f51a73fd0799b9bded12abc0ca07ce6a3a497
tharamesseroux <messeroux.t@northeastern.edu>
2022-02-25 21:26:26 -0500

    a5 update

--- public/tuiter/WhoToFollowList/who.js
+++ public/tuiter/WhoToFollowList/who.js
@@ -1,25 +1,25 @@
 export default [{
-        avatarIcon: '../../images/java.png',
+        avatarIcon: '../images/java.png',
         userName: 'Java',
         handle: 'Java',
     },
     {
-        avatarIcon: '../../images/relativity.jpg',
+        avatarIcon: '../images/relativity.jpg',
         userName: 'Relativity Space',
         handle: 'relativityspace',
     },
     {
-        avatarIcon: '../../images/glactic.jpg',
+        avatarIcon: '../images/glactic.jpg',
         userName: 'Virgin Galactic',
         handle: 'virgingalactic',
     },
     {
-        avatarIcon: '../../images/nasa.png',
+        avatarIcon: '../images/nasa.png',
         userName: 'NASA',
         handle: 'NASA',
     },
     {
-        avatarIcon: '../../images/tesla.png',
+        avatarIcon: '../images/tesla.png',
         userName: 'Tesla',
         handle: 'Tesla',
     },
