13a984f3e06e7c54a7d27a1c133e633f70a05ae6
tharamesseroux <messeroux.t@northeastern.edu>
2022-03-01 15:49:33 -0500

    a6 React.js sp22

--- public/tuiter/PostSummaryList/index.js
+++ public/tuiter/PostSummaryList/index.js
@@ -1,11 +1,11 @@
-import PostSumaryItem from "./PostSummaryItem.js";
+import PostSummaryItem from "./PostSummaryItem.js";
 import post from "./post.js";
 
 const PostSummaryList = () => {
     return(`
         <div class='suggested-posts'>
             ${post.map(post => {
-                return PostSumaryItem(post);
+                return PostSummaryItem(post);
             }).join('')
             }
         </div>
