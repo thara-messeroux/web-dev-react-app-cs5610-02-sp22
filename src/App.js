46e65a16bb00918b3642912ba8735b2df56cb42a
tharamesseroux <messeroux.t@northeastern.edu>
2022-03-01 21:09:14 -0500

    Apps to be updated

--- src/App.js
+++ src/App.js
@@ -1,32 +1,57 @@
-// function App() {
-//   return (
-//       <h1>Hello World!</h1>
-//   );
-// }
-// export default App;
-
-import logo from './logo.svg';
+import './vendors/bootstrap/css/bootstrap.min.css';
+import './vendors/bootstrap/bootswatch/bootstrap.min.css';
+import './vendors/fontawesome/css/all.min.css';
 import './App.css';
+import Labs from "./components/Labs";
+import Hello from "./components/Hello";
+import Tuiter from "./components/tuiter";
+import {BrowserRouter} from "react-router-dom";
+
+
+// import {Component} from "react";
+//
+// class Tuiter extends Component {
+//   render() {
+//     return null;
+//   }
+// }
 
 function App() {
   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <BrowserRouter>
+      <div className="container">
+        <Hello/>
+        <Labs/>
+        <Tuiter/>
       </div>
+        <BrowserRouter>
   );
 }
+export default App;
 
+
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//               className="App-link"
+//               href="https://reactjs.org"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//   );
+// }
+//
 // export default App;
