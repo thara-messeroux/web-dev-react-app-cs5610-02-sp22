8cc9384841472efcc1290fa30401e2293ec619e9
tharamesseroux <messeroux.t@northeastern.edu>
2022-04-06 20:36:19 -0400

    Updated url

--- src/components/Tuiter/services/tuits-service.js
+++ src/components/Tuiter/services/tuits-service.js
@@ -1,25 +1,24 @@
 import axios from 'axios';
-const TUITS_API = 'https://full-stack-node-thara.herokuapp.com/api/users';
+
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
-  const response = await axios.post(TUITS_API, tuit)
+  const response = await axios.post(TUITS_API, tuit);
   return response.data;
-}
-
+};
 export const findAllTuits = async () => {
-  return await axios.get(TUITS_API)
-  .then((response)=>response.data)
-  .then((data)=>data);
-}
-
+  const response = await axios.get(TUITS_API);
+  const tuits = response.data;
+  return tuits;
+};
 export const deleteTuit = async (tuit) => {
   const response = await axios
   .delete(`${TUITS_API}/${tuit._id}`);
   return response.data;
-}
-
+};
 export const updateTuit = async (tuit) => {
   const response = await axios
   .put(`${TUITS_API}/${tuit._id}`, tuit);
   return response.data;
-}
+};
